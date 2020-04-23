92e9c36acd604e3e09f6116cfbfa546371711459
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-23 11:16:13 +0530

    food billing producer added

---
+++ FoodBillingProducer/src/foodbillingproducer/Activator.java
@@ -0,0 +1,14 @@
+package foodbillingproducer;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Activator implements BundleActivator {
+
+	ServiceRegistration publishServiceRegistration;
+
+	
+
+	
+}
