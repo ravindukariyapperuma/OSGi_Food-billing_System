350560e2a6924534f78269e9e9fd3b2f0fcf5e49
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-23 11:08:25 +0530

    food billing consumer added

---
+++ FoodBillingConsumer/src/foodbillingconsumer/Activator.java
@@ -0,0 +1,17 @@
+package foodbillingconsumer;
+
+import foodbillingproducer.FoodBillingService;
+import foodbillingproducer.FoodBillingServiceImpl;
+
+import java.util.Scanner;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+public class Activator implements BundleActivator {
+
+	ServiceReference serviceReference;
+
+	
+}
